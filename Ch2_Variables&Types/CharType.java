c63715158cc5793d6446f1cf66165daed8590969
sychoi25 <48307649+sychoi25@users.noreply.github.com>
2020-05-03 04:22:46 +0900

    Add files via upload

---
+++ Ch2_Variables&Types/CharType.java
@@ -0,0 +1,19 @@
+package ch2_VariablesTypes;
+
+public class CharType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char c1 = 'a';
+		char c2 = 65;
+		
+		int getUnicode = c1; //get Unicode of var c1
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(getUnicode);
+	}
+
+
+}
